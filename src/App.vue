5e0cb830f87240aea42e6e5eca6ae57fbef99948
cindychang-findingstech <cindy.chang@findingstech.com>
2025-07-02 10:32:45 +0800

    fix - modal display bug. Destructure cancles out reactivity, so we have to use modal.isOpen instead of isOpen

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import Modal from '@/components/common/Modal.vue'
+import { useModalStore } from '@/stores/modalStore'
+
+const modal = useModalStore()
+
 </script>
 
 <template>
--- src/components/common/Modal.vue
+++ src/components/common/Modal.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    v-if="isOpen"
+    v-if="modal.isOpen"
     class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
   >
     <div class="bg-white rounded-xl p-6 w-[90%] max-w-md text-center shadow-xl">
       <p class="text-lg mb-4 text-gray-800">{{ message }}</p>
       <button
         class="bg-blue-600 text-white px-6 py-2 rounded-xl"
-        @click="closeModal"
+        @click="modal.closeModal"
       >知道了</button>
     </div>
   </div>
