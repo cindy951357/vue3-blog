ce3eabbb64666db86bd746a4278df01d95afb2cd
cindychang-findingstech <cindy.chang@findingstech.com>
2025-07-03 08:52:26 +0800

    add - focus effect

--- src/components/main/HomeFeed.vue
+++ src/components/main/HomeFeed.vue
@@ -9,13 +9,13 @@ import RightPanel from './RightPanel.vue';
       <div class="flex gap-x-4 mb-4 border-b pb-2">
         <RouterLink
           to="/recommend/posts"
-          class="hover:text-rose-300"
+          class="hover:text-rose-300 focus:text-rose-300"
           :class="{ 'font-bold text-neutral-300': $route.path.startsWith('/recommend/posts') }"
         >推薦文章</RouterLink>
 
         <RouterLink
           to="/myposts/posts"
-          class="hover:text-rose-300"
+          class="hover:text-rose-300 focus:text-rose-300"
           :class="{ 'font-bold text-neutral-300': $route.path.startsWith('/myposts/posts') }"
         >我的文章</RouterLink>
       </div>
