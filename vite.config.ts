de47d36cd121981634819b28417ca684295710d5
cindychang-findingstech <cindy.chang@findingstech.com>
2025-07-05 17:10:52 +0800

    add - vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import tailwindcss from '@tailwindcss/vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +12,17 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    viteSingleFile(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    minify: false,
+    // target: 'esnext',
+    // assetsInlineLimit: Infinity, // 將所有資源內嵌
+    // cssCodeSplit: false,         // CSS 不分離
+  }
 })
